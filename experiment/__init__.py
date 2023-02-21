653b5770c85dd4fb439c65f804bc9fc0305ffbba
andrearafanelli <andrea.rafanelli96@gmail.com>
2023-02-21 10:29:07 +0100

    fix: csv results saving

--- experiment/__init__.py
+++ experiment/__init__.py
@@ -295,8 +295,11 @@ def run_experiments(datasets: list[SpliceJunction or BreastCancer or CensusIncom
                     results_inference.loc[injector_name, metric] = compute_metrics_inference(uneducated, educated, params)
 
         if update:
-            results_training.to_csv(RESULTS_PATH / (dataset.name + '_metrics_training.csv'), mode='+', float_format='%.2f')
-            results_inference.to_csv(RESULTS_PATH / (dataset.name + '_metrics_inference.csv'), mode='+', float_format='%.2f')
+            read_training = pd.read_csv(RESULTS_PATH / (dataset.name + '_metrics_training.csv'))
+            read_inference = pd.read_csv(RESULTS_PATH / (dataset.name + '_metrics_inference.csv'))
+            read_training.update(results_training.astype(float).round(2))
+            read_inference.update(results_inference.astype(float).round(2))
+
         else:
-            results_training.to_csv(RESULTS_PATH / (dataset.name + '_metrics_training.csv'), float_format='%.2f')
-            results_inference.to_csv(RESULTS_PATH / (dataset.name + '_metrics_inference.csv'), float_format='%.3f')
+            results_training.astype(float).round(2).to_csv(RESULTS_PATH / (dataset.name + '_metrics_training.csv'))
+            results_inference.astype(float).round(2).to_csv(RESULTS_PATH / (dataset.name + '_metrics_inference.csv'))
